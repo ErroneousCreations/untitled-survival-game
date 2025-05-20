04e0c495326688207e20b2699883cd2250c0018e
ErroneousCreationiost <76930150+ErroneousCreationist@users.noreply.github.com>
2025-05-21 09:54:27 +1000

    ball bouncy, work on creature targeting

--- Assets/Scripts/AITarget.cs
+++ Assets/Scripts/AITarget.cs
@@ -6,6 +6,8 @@ public class AITarget : MonoBehaviour
     public static List<AITarget> targets = new List<AITarget>();
     public string TargetType;
 
+    [EditorAttributes.ReadOnly]public float ScarinessModifier = 1, AggroModifier = 1, AudioEnergy = 0;
+
     private void OnEnable()
     {
         targets.Add(this);
--- Assets/Scripts/AITargeter.cs
+++ Assets/Scripts/AITargeter.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Collections;
 using EditorAttributes;
 using UnityEngine.Events;
+using AYellowpaper.SerializedCollections;
 
 public class AITargeter : MonoBehaviour
 {
+    public struct RelationshipStruct
+    {
+        public float Fear, Aggression;
+        public bool Ignore;
+    }
+
     public bool CanSee = true;
     [ShowField(nameof(CanSee))] public Transform EyesPosition;
     [ShowField(nameof(CanSee))] public float ViewRange, ViewAngle;
@@ -16,26 +23,47 @@ public class AITargeter : MonoBehaviour
     [ShowField(nameof(CanHear))] public UnityEvent<float, Vector3> HeardSound;
     public System.Action<float, Vector3> OnHeardSound;
 
-    public static List<AITargeter> listeners = new List<AITargeter>();  
+    [ReadOnly]public float FearModifier = 1, AggressionModifier = 1;
 
-    void OnEnable()
-    {
-        if (!CanHear) return;
-        listeners.Add(this);
-    }
+    public RelationshipStruct DefaultRelationship;
+    public SerializedDictionary<string, RelationshipStruct> Relationships;
 
-    void OnDisable()
-    {
-        if (!CanHear) return;
-        listeners.Remove(this);
-    }
+    private List<AITarget> aggressiveTo, scaredOf;
 
-    public static void SoundEmitted(float intensity, Vector3 position)
+    public List<AITarget> GetAggroTargets => aggressiveTo;
+    public List<AITarget> GetScaredTargets => scaredOf;
+
+    public AITarget TopAggroTarget => aggressiveTo.Count > 0 ? aggressiveTo[0] : null;
+    public AITarget TopScaredTarget => scaredOf.Count > 0 ? scaredOf[0] : null;
+
+    public void FindTarget()
     {
-        foreach (var targeter in listeners)
+        aggressiveTo.Clear();
+        scaredOf.Clear();
+        Dictionary<AITarget, Vector3> foundvalues = new();
+
+        foreach (var target in AITarget.targets)
         {
-            targeter.OnHeardSound?.Invoke(intensity, position);
-            targeter.HeardSound?.Invoke(intensity, position);
+            var relation = DefaultRelationship;
+            if (Relationships.ContainsKey(target.TargetType)) { relation = Relationships[target.TargetType]; }
+            if (relation.Ignore) { continue; }
+
+            var dist = (target.transform.position - transform.position).sqrMagnitude;
+            var dir = (target.transform.position - EyesPosition.transform.position).normalized;
+            //seeing is believing
+            if (CanSee && dist < ViewRange*ViewRange &&
+                Vector3.Angle(dir, EyesPosition.transform.forward) < ViewAngle &&
+                Physics.Raycast(EyesPosition.position, dir, out RaycastHit hit, Mathf.Sqrt(dist), ViewMask, QueryTriggerInteraction.Ignore) && hit.transform == target.transform) {
+                var fear = relation.Fear * FearModifier * target.ScarinessModifier;
+                var aggression = relation.Aggression * AggressionModifier * target.AggroModifier;
+                if (fear > aggression) { scaredOf.Add(target); }
+                else { aggressiveTo.Add(target); }
+                foundvalues.Add(target, new Vector3(dist, fear, aggression));
+            }
+
+            if(CanHear && dist < HearRange && (target.AudioEnergy / (1 + (dist * 0.1f))) > HearingPower) {
+
+            }
         }
     }
 }
